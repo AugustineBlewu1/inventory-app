50779b09d856f7c76eeb7ed3fe0c25d1359a803f
AugustineBlewu1 <blewudziaugustine.com>
2025-01-10 19:51:53 +0000

    feat: updated

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { Bell, Menu, Settings, Sun } from "lucide-react";
+import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import { setIsDarkMode, setIsSidebarCollapsed } from "@/app/state";
@@ -42,7 +42,7 @@ const Navbar = (props: Props) => {
             name=""
             id=""
             placeholder="Start type to search groups & products"
-            className="pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rouded-lg focus:outline-none focus:border-blue-500"
+            className="pl-10 pr-4 py-2 w-50 md:w-70 border-2 border-gray-300 bg-white rouded-lg focus:outline-none focus:border-blue-500"
           />
 
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none ">
@@ -57,7 +57,14 @@ const Navbar = (props: Props) => {
           <div>
             
             <button onClick={toggleDarkMode}>
-              <Sun className="cursor-pointer text-gray-500" size={24} />
+              {
+                isDarkMode ? (
+                  <Sun className="cursor-pointer text-gray-500" size={24} />
+
+                ): (
+                  <Moon className="cursor-pointer text-gray-500" size={24}  />
+                )
+              }
             </button>
           </div>
           <div className="relative">
